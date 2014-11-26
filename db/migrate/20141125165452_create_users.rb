e12dfb37bbcae357798c48e4bc215520ed3e1d61
vagrant <vagrant@ironhack>
2014-11-26 17:03:18 +0000

    migrated

--- db/migrate/20141125165452_create_users.rb
+++ db/migrate/20141125165452_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
+     	t.integer :profile_id
+    	t.string :portfolio_url
+    	
 
       t.timestamps
     end
